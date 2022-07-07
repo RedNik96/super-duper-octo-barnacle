2196a244013591bab436876b113ce97f82b7a78e
Niklas Devenish <15998962+RedNik96@users.noreply.github.com>
2022-07-07 12:39:52 +0200

    Add general meta infos to API (#4)
    * add general metadata to API
    
    * pylint C0103: change description naming style to match Constant

--- super_duper_octo_barnacle/main.py
+++ super_duper_octo_barnacle/main.py
@@ -1,6 +1,20 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+DESCRIPTION = """
+Bring your shopping list to the cloud. 🚀
+
+You will be able to:
+
+* create & share shopping list (_not implemented_).
+  * share lists via links (that include a GUID) so there is no nee to sign up (_not implemented_).
+* sign up
+  * create recurrings shopping lists/items (_not implemented_).
+  * list all items ever bougth (_not implemented_).
+  * utilizing shopping history for a type ahead (_not implemented_).
+  * make an inventory to know what you have got at home (_not implemented_).
+"""
+
+app = FastAPI(title="Shopping list", description=DESCRIPTION, version="0.0.1")
 
 
 @app.get("/")
